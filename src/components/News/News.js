4ffaa322ec20840461842a4cb8d881fe46e56aa4
meghanduprey <meghan.duprey@gmail.com>
2019-11-12 11:22:27 -0500

    have working prototype

---
+++ src/components/News/News.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import NewSingle from './NewSingle';
+
+class News extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=8222f155add24709bfa464acfe42bf51`;
+
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.setState({
+        news: data.articles
+      })
+    })
+    .catch((error) => console.log(error));
+  }
+
+  renderItems() {
+    return this.state.news.map((item) => (
+      <NewSingle key={item.url} item={item} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.renderItems()}
+      </div>
+    )
+  }
+}
+
+export default News;
